12911b69a0223666bbd343c97ab3fa96dea4adc5
sarahbroton <sarah.broton@gmail.com>
2023-07-05 21:19:50 -0500

    lastcommitfortoday

--- index.js
+++ index.js
@@ -12,77 +12,77 @@ const fs = require('fs');
 const questions = ([
     {
         type: 'input',
-        message: 'What is the title of your project?', 
+        message: 'What is the title of your project?',
         name: 'Project title'
     },
     {
-        type: 'input', 
-        message: 'What are title of your sections?', 
+        type: 'input',
+        message: 'What are title of your sections?',
         name: 'Section title(s)'
 
-    }, 
+    },
     {
-        type: 'input', 
-        message: 'Write a description of your project', 
+        type: 'input',
+        message: 'Write a description of your project',
         name: 'Project Description'
-    }, 
+    },
     {
-        type: 'input', 
-        message: 'If applicable, enter in your table of contents.  If none, type in N/A', 
+        type: 'input',
+        message: 'If applicable, enter in your table of contents.  If none, type in N/A',
         name: 'Table of Contents'
-    }, 
+    },
     {
-        type: 'input', 
-        message: 'If applicable, describe the steps required to install your project for the Installation section. If none, type in N/A.', 
+        type: 'input',
+        message: 'If applicable, describe the steps required to install your project for the Installation section. If none, type in N/A.',
         name: 'Installation'
-    }, 
+    },
     {
-        type: 'input', 
-        message: 'Provide instructions and examples of your project for the Usage section.', 
+        type: 'input',
+        message: 'Provide instructions and examples of your project for the Usage section.',
         name: 'Usage'
-    }, 
+    },
     {
-        type: 'list', 
-        message: 'Choose a licence for this project.', 
-        choices: ['MIT', 'Educational Community Licence v 2.0', 'Do What the F*ck You Want to Public License', 'Artistic License 1.0', 'Creative Commons license family', 'Creative Commons Attribution 4.0', 'Creative Commons Attribution Share Alike 4.0', 'Academic Free LIcense v 3.0'], 
+        type: 'list',
+        message: 'Choose a licence for this project.',
+        choices: ['MIT', 'Educational Community Licence v 2.0', 'Do What the F*ck You Want to Public License', 'Artistic License 1.0', 'Creative Commons license family', 'Creative Commons Attribution 4.0', 'Creative Commons Attribution Share Alike 4.0', 'Academic Free LIcense v 3.0'],
         name: 'License'
-    }, 
+    },
     {
-        type: 'input', 
-        message: 'If applicable, provide guidelines on how other developes can contribute to your project.', 
+        type: 'input',
+        message: 'If applicable, provide guidelines on how other developes can contribute to your project.',
         name: 'Contributing'
-    }, 
-    {   
-        type: 'input', 
-        message: 'If applicable, provide any tests written for your application and provide examples on how to run them.', 
-        name: 'Tests'
-    }, 
+    },
     {
-        type: 'input', 
-        message: 'Any questions', 
+        type: 'input',
+        message: 'If applicable, provide any tests written for your application and provide examples on how to run them.',
+        name: 'Tests'
+    },
+    {
+        type: 'input',
+        message: 'Any questions',
         name: 'Questions'
     }
 
+])
 
-]);
-
-
-// ITEMS TO BE INCLUDED IN THE QUESTIONS
-// title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+    .then((response)) =>
+response.confirm === response.password
+    ? console.log('Sucess')
+    : console.log('You forgot');
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+// function writeToFile(fileName, data) { }
 
-// function to initialize program
-function init() {
-    // TODO: Create a function to initialize app
-    function init() { }
+// // function to initialize program
+// function init() {
+//     // TODO: Create a function to initialize app
+//     function init() { }
 
-}
+// }
 
-// function call to initialize program
-// Function call to initialize app
-init();
+// // function call to initialize program
+// // Function call to initialize app
+// init();
