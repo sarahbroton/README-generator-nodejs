40889f985a3aa79b9470b56d0f204f9ac1acea15
sarahbroton <sarah.broton@gmail.com>
2023-07-14 18:39:27 -0500

    readmelicencesection

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -3,9 +3,9 @@
 // If there is no license, return an empty string
 
 function renderLicenseBadge(license) {
-  console.log(license); 
+  console.log(license);
   let yourLicense = ''
-  if(license === 'MIT') {
+  if (license === 'MIT') {
     yourLicense = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
   } else if (license === 'Do What the F*uck You Want to Public License') {
     yourLicense = '![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)'
@@ -15,61 +15,68 @@ function renderLicenseBadge(license) {
     yourLicense = '![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)'
   } else if (license === 'The Unlicense') {
     yourLicense = '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)'
-  } 
-  
-  return yourLicense; 
+  }
+
+  return yourLicense;
 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+class generateMarkdown {
+  static generateReadme(data) {
+    return `# $(data.title)`
+  }
+}
 
-function renderLicenseSection(license) {}
+
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  
+  return `# ${data.title}`
+}
+ 
   ## Licence
-  ${renderLicenseBadge(data.license)}
+  ${ renderLicenseBadge(data.license) }
 
   ## Table of Contents
-  1. [Description](#description)
-  2. [Installation](#installation)
-  3. [Usage](#usage)
-  4. [Credits](#credits)
-  5. [Licence](#licence)
-  6. [Contributions](#contributions)
-  7. [Tests](#tests)
-  8. [Questions](#questions)
+  1.[Description](#description)
+  2.[Installation](#installation)
+  3.[Usage](#usage)
+  4.[Credits](#credits)
+  5.[Licence](#licence)
+  6.[Contributions](#contributions)
+  7.[Tests](#tests)
+  8.[Questions](#questions)
 
   ## Description
-  ${data.description}
+  ${ data.description }
 
   ## Installation
-  ${data.installation}
+  ${ data.installation }
 
   ## Usage
-  ${data.usage}
+  ${ data.usage }
 
   ## Credits
-  ${data.credits}
+  ${ data.credits }
 
   ## Contributions
-  ${data.contributions}
+  ${ data.contributions }
 
   ## Tests
-  ${data.tests}
+  ${ data.tests }
 
   ## Questions
-  Any questions should be directed to: ${data.ghusername}
+  Any questions should be directed to: ${ data.ghusername }
   // Your questions: ${data.questions}
 
-`;
+  `;
 }
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -45,7 +45,10 @@ questions = [
         type: 'list',
         message: 'Choose a licence for this project.',
         choices: ['MIT', 'Do What the F*uck You Want to Public License', 'Artistic License 2.0', 'Creative Commons Attribution Share Alike 4.0', 'The Unlicense'],
-        name: 'license'
+        name: 'license', 
+        filter (val) {
+            return val.toLowerCase(); 
+        }
     },
     {
         type: 'input',
